94735ab8d2e5129cf65a77a91d009667f2606d18
Aaron Howser <aaronhowser1@gmail.com>
2023-01-25 09:24:43 -0600

    Started mass

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -2,34 +2,64 @@ package converter
 
 fun main() {
 
-    val acceptedUnits = arrayOf("m","meter","meters","km","kilometer","kilometers","cm","centimeter","centimeters","mm","millimeter","millimeters","mi","mile","miles","yd","yard","yards","ft","foot","feet","in","inch","inches")
+    val lengthUnits = arrayOf("m","meter","meters","km","kilometer","kilometers","cm","centimeter","centimeters","mm","millimeter","millimeters","mi","mile","miles","yd","yard","yards","ft","foot","feet","in","inch","inches")
+    val massUnits = arrayOf("g","gram","grams","kg","kilogram","kilograms","mg","milligram","milligrams","lb","pound","pounds","oz","ounce","ounces")
 
-    println("Enter a number and a measure of length:")
+    println("Enter what you want to convert (or exit): ")
     val input = readln().split(" ")
     val amount = input[0].toDouble()
-    var unit = input[1].lowercase()
+    var inputUnit = input[1].lowercase()
+    var outputUnit = input[3].lowercase()
 
-    if (acceptedUnits.contains(unit)) {
-        unit = clarifyUnitName(unit,amount)
-        val convertedAmount = convertToMeters(amount,unit)
-        val convertedUnit = clarifyUnitName("meter",convertedAmount)
-
-        println("$amount $unit is $convertedAmount $convertedUnit")
+    if (lengthUnits.contains(inputUnit)) {
+        if (lengthUnits.contains(outputUnit)) {
+            inputUnit = clarifyUnitName(inputUnit,amount)
+            val amountInMeters = convertToMeters(amount,inputUnit)
+            val convertedUnit = clarifyUnitName("meter",amountInMeters)
+            println("$amount $inputUnit is $amountInMeters $convertedUnit")
+        } else {
+            if (massUnits.contains(outputUnit)) {
+                println("Conversion from ${makePlural(inputUnit)} to ${makePlural(outputUnit)} is impossible")
+            } else println("Wrong output. Unknown unit $outputUnit")
+        }
     } else {
-        println("Wrong input. Unknown unit $unit")
+        if (massUnits.contains(inputUnit)) {
+            if (massUnits.contains(outputUnit)) {
+                inputUnit = clarifyUnitName(inputUnit,amount)
+
+            }
+            if (lengthUnits.contains(outputUnit)) {
+                println("Conversion from ${makePlural(inputUnit)} to ${makePlural(outputUnit)} is impossible")
+            } else println("Wrong output. Unknown unit $outputUnit")
+        }
+        else println("Wrong input. Unknown unit $inputUnit")
     }
 }
 
 fun convertToMeters(amount: Double, unit: String): Double {
     return when (unit) {
-        "m","meter","meters" -> amount
-        "km","kilometer","kilometers" -> amount*1000
-        "cm","centimeter","centimeters" -> amount*0.01
-        "mm","millimeter","millimeters" -> amount*0.001
-        "mi","mile","miles" -> amount*1609.35
-        "yd","yard","yards" -> amount*0.9144
-        "ft","foot","feet" -> amount*0.3048
-        "in","inch","inches" -> amount*0.0254
+        "meter","meters" -> amount
+        "kilometer","kilometers" -> amount*1000
+        "centimeter","centimeters" -> amount*0.01
+        "millimeter","millimeters" -> amount*0.001
+        "mile","miles" -> amount*1609.35
+        "yard","yards" -> amount*0.9144
+        "foot","feet" -> amount*0.3048
+        "inch","inches" -> amount*0.0254
+        else -> 0.0
+    }
+}
+
+fun convertFromMeters(amount: Double, unit: String): Double {
+    return when (unit) {
+        "meter" -> amount
+        "kilometer" -> amount/1000
+        "centimeter" -> amount/0.01
+        "millimeter" -> amount/0.001
+        "mile" -> amount/1609.35
+        "yard" -> amount/0.9144
+        "foot" -> amount/0.3048
+        "inch" -> amount/0.0254
         else -> 0.0
     }
 }
@@ -44,14 +74,29 @@ fun clarifyUnitName(unit: String, amount: Double): String {
         "yd", "yard", "yards" -> "yard"
         "ft", "foot", "feet" -> "foot"
         "in", "inch", "inches" -> "inch"
-        else -> ""
+
+        "g", "gram", "grams" -> "gram"
+        "kg","kilogram", "kilograms" -> "kilogram"
+        "mg", "milligram", "milligrams" -> "milligram"
+        "lb", "pound", "pounds" -> "pound"
+        "oz", "ounce", "ounces" -> "ounce"
+
+        else -> unit
     }
     if (amount != 1.0) {
-        when (newUnit) {
-            "foot" -> newUnit = "feet"
-            "inch" -> newUnit += "es"
-            else -> newUnit += 's'
-        }
+        newUnit = makePlural(newUnit)
     }
     return newUnit
+}
+
+fun makePlural(input: String): String {
+    var output = input
+    if (output != "feet" && output != "inches" && output.last() != 's') {
+        when (output) {
+            "foot" -> output = "feet"
+            "inch" -> output += "es"
+            else -> output += 's'
+        }
+    }
+    return output
 }
