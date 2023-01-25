a42192475f912538af1379acccd3e9f4cc81104e
Aaron Howser <aaronhowser1@gmail.com>
2023-01-25 04:50:17 -0600

    2

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,11 +1,34 @@
 package converter
 
+val centimetersToMeters = Pair(145.0, 1.45)
+val milesToKilometers = Pair(2.0, 3.2187)
+val inchesToMillimeters = Pair(5.5, 139.7)
+val celsiusToFahrenheit = Pair(12.0, 53.6)
+val poundsToKilograms = Pair(3.0,1.360776)
+
 fun main() {
-    println("""
-        145 centimeters is 1.45 meters
-        2 miles is 3.2187 kilometers
-        5.5 inches is 139.7 millimeters
-        12 degrees Celsius is 53.6 degrees Fahrenheit
-        3 pounds is 1.360776 kilograms
-    """.trimIndent())
+    println("Enter a number and a measure:")
+    val input = readln().split(" ")
+    val amount = input[0].toDouble()
+    val unit = input[1].lowercase()
+
+    val convertedAmount: Double = when (unit) {
+        "cm","centimeters" -> convert(centimetersToMeters, amount)
+        "m","meters" -> convert(centimetersToMeters, amount, true)
+        "mi","miles" -> convert(milesToKilometers, amount)
+        "km","kilometers" -> convert(milesToKilometers, amount)
+        "in","inches" -> convert(inchesToMillimeters, amount)
+        "mm","millimeters" -> convert(inchesToMillimeters, amount, true)
+        "c","celsius" -> convert(celsiusToFahrenheit, amount)
+        "f","fahrenheit" -> convert(celsiusToFahrenheit, amount, true)
+        "lb","pounds" -> convert(poundsToKilograms, amount)
+        "kg","kilograms" -> convert(poundsToKilograms, amount, true)
+        else -> 0.0
+    }
+
+}
+
+fun convert(pair: Pair<Double, Double>, amount: Double, firstToSecond: Boolean = true): Double {
+    val factor = if (firstToSecond) pair.first/pair.second else pair.second/pair.first
+    return amount*factor
 }
